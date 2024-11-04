ffc3aa952705a67a65b590606182cf977c256b8a
roselyne mwaniki <roselynewairimu30@gmail.com>
2024-11-04 14:35:51 +0300

    finally changed

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
-import About from "./About";
+import About from './About';
 import ProjectList from "./ProjectList";
 
 
@@ -11,12 +11,16 @@ function App() {
     { id: 2, name: "Project 2", about: "Description 2", technologies: ["React", "Redux", "Node.js"] },
     { id: 3, name: "Project 3", about: "Description 3", technologies: ["Ruby", "Rails", "PostgreSQL"] },
   ];
+  const links = {
+    github: "https://github.com/yourusername",
+    linkedin: "https://linkedin.com/in/yourusername",
+  };
 
   return (
     <div>
       <NavBar />
       <Home />
-      <About />
+      <About links={links}/>
       <ProjectList projects={projects} />
     </div>
   );
